b9f1ff241b81ba226f5796021827b135151aca71
Solomon Antony <santony@murraystate.edu>
2025-08-25 11:22:15 -0500

    done with task 1

---
+++ Lab1-Statements/task1.py
@@ -0,0 +1,7 @@
+name = input('What is your name?')
+print('Hello', name)
+
+first_name = input('Enter first name: ')
+last_name = input('Enter last name: ')
+greeting = 'Hello,' + first_name + ' ' + last_name
+print(greeting)
