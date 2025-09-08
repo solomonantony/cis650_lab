c8bf903b1c86644001a8b60d886ca7e8c88f0825
Solomon Antony <santony@murraystate.edu>
2025-09-08 11:49:59 -0500

    after class sessionn 9/8/2025

--- Lab3-DataStructures/Dictionaries/03_iteration.py
+++ Lab3-DataStructures/Dictionaries/03_iteration.py
@@ -1,8 +1,9 @@
 list1 = ['a', 'a', 'c', 'b']
 list2 = [1,2,5,7]
 dict1 = dict(zip(list1, list2))
-dict1
+print(dict1)
 for k in dict1:  #when we don't know  the key labels
+  print(type(k))
   print(k, dict1[k])
 
 for key, value in dict1.items(): #when we don't know  the key labels
--- Lab3-DataStructures/Dictionaries/04_adding.py
+++ Lab3-DataStructures/Dictionaries/04_adding.py
@@ -3,15 +3,18 @@ for month, days in days_per_month.items():
     print(f'{month} has {days} days')
 
 #Accessing the Value Associated with a Key;  Use square brackets
-days_per_month['January']
+print(days_per_month['January'])
 
 #Updating the Value of an Existing Key–Value Pair
 days_per_month['February'] = 29
-
+print(days_per_month)
 #Adding a New Key–Value Pair
 days_per_month['April'] = 30
-days_per_month
+print(days_per_month)
+print(days_per_month.get('May'))
 
+
+#print(days_per_month['May'])
 #check if a key exixsts before accessing it
 if not days_per_month.get('May'):
   print('May not found')
