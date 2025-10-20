0c82c507529963cac3cd60ef4006e2f2e3467af1
Solomon Antony <santony@murraystate.edu>
2025-10-20 11:43:40 -0500

    after class 10/20/2025

--- Lab7-FileOperations/11_error.py
+++ Lab7-FileOperations/11_error.py
@@ -3,5 +3,5 @@ def printdir(dir):
   filenames = os.listdir(dir)
   for filename in filenames:
     print('file name: ',filename)  
-printdir('C:/Users/santony/Document/')
+printdir('C:/Users/antony/Document/')
 #passing an incorrect path to the function leads to a runtime error on line 3
